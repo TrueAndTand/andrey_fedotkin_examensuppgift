b4184bcb7a8e21526737849f67f17d25128ddd5d
TrueAndTand <levakk124@gmail.com>
2026-06-03 09:58:21 +0200

    Ver 4

---
+++ js/script.js
@@ -0,0 +1,67 @@
+document.addEventListener("keydown", (event) => {
+
+    if(event.key == "Escape"){
+        document.getElementById("esc").style = "color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "F1"){
+        document.getElementById("f1").style = "color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "F2"){
+        document.getElementById("f2").style = "color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "F3"){
+        document.getElementById("f3").style = "color:coral; border-color:coral;"
+    }    
+    
+    else if(event.key == "F4"){
+        document.getElementById("f4").style = "color:coral; border-color:coral;"
+    }    
+    
+    else if(event.key == "F5"){
+        document.getElementById("f5").style = "color:coral; border-color:coral;"
+    }    
+    
+    else if(event.key == "F6"){
+        document.getElementById("f6").style = "color:coral; border-color:coral;"
+    }    
+    
+    else if(event.key == "F7"){
+        document.getElementById("f7").style = "color:coral; border-color:coral;"
+    }    
+    
+    else if(event.key == "F8"){
+        document.getElementById("f8").style = "color:coral; border-color:coral;"
+    }    
+    
+    else if(event.key == "F9"){
+        document.getElementById("f9").style = "color:coral; border-color:coral;"
+    }    
+    
+    else if(event.key == "F10"){
+        document.getElementById("f10").style = "color:coral; border-color:coral;"
+    }    
+    
+    else if(event.key == "F11"){
+        document.getElementById("f11").style = "color:coral; border-color:coral;"
+    }    
+    
+    else if(event.key == "F12"){
+        document.getElementById("f12").style = "color:coral; border-color:coral;"
+    }    
+    
+    else if(event.key == "PrintScreen"){
+        document.getElementById("prt").style = "color:coral; border-color:coral;"
+    }    
+    
+    else if(event.key == "Power"){
+        document.getElementById("pwr").style = "color:coral; border-color:coral;"
+    }    
+    
+    else if(event.key == "Delete"){
+        document.getElementById("del").style = "color:coral; border-color:coral;"
+    }
+    
+});
