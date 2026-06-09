2324bc4120899974d3165555a84a9f7f0d5f3300
TrueAndTand <levakk124@gmail.com>
2026-06-09 15:43:52 +0200

    ver 4 idk??

--- js/script.js
+++ js/script.js
@@ -64,10 +64,479 @@ document.addEventListener("keydown", (event) => {
         document.getElementById("del").style = "color:coral; border-color:coral;"
     }
     
+    else if(event.key == "§"){
+        document.getElementById("§").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "1"){
+        document.getElementById("1").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "2"){
+        document.getElementById("2").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "3"){
+        document.getElementById("3").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "4"){
+        document.getElementById("4").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "5"){
+        document.getElementById("5").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "6"){
+        document.getElementById("6").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "7"){
+        document.getElementById("7").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "8"){
+        document.getElementById("8").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "9"){
+        document.getElementById("9").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "0"){
+        document.getElementById("0").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "+"){
+        document.getElementById("plus").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == ""){
+        document.getElementById("accent").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "Backspace"){
+        document.getElementById("bck").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "Home"){
+        document.getElementById("home").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "Tab"){
+        document.getElementById("tab").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "q"){
+        document.getElementById("q").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "w"){
+        document.getElementById("w").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "e"){
+        document.getElementById("e").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "r"){
+        document.getElementById("r").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "t"){
+        document.getElementById("t").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "y"){
+        document.getElementById("y").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "u"){
+        document.getElementById("u").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "i"){
+        document.getElementById("i").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "o"){
+        document.getElementById("o").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "p"){
+        document.getElementById("p").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "å"){
+        document.getElementById("å").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == ""){
+        document.getElementById("¨").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "Enter"){
+        document.getElementById("enter").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "PageUp"){
+        document.getElementById("pg_up").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "CapsLock"){
+        document.getElementById("cps_lck").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "a"){
+        document.getElementById("a").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "s"){
+        document.getElementById("s").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "d"){
+        document.getElementById("d").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "f"){
+        document.getElementById("f").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "g"){
+        document.getElementById("g").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "h"){
+        document.getElementById("h").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "j"){
+        document.getElementById("j").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "k"){
+        document.getElementById("k").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "l"){
+        document.getElementById("l").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "ö"){
+        document.getElementById("ö").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "ä"){
+        document.getElementById("ä").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "'"){
+        document.getElementById("apostrof").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "PageDown"){
+        document.getElementById("pg_dn").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "ShiftLeft"){
+        document.getElementById("LShift").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "|"){
+        document.getElementById("|").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "z"){
+        document.getElementById("z").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "x"){
+        document.getElementById("x").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "c"){
+        document.getElementById("c").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "v"){
+        document.getElementById("v").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "b"){
+        document.getElementById("b").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "n"){
+        document.getElementById("n").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "m"){
+        document.getElementById("m").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "Comma"){
+        document.getElementById(",").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "Period"){
+        document.getElementById(".").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "Minus"){
+        document.getElementById("-").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "Shift"){
+        document.getElementById("RShift").style ="color:coral; border-color:coral;"
+    }
+
+    else if(event.key == "end"){
+        document.getElementById("end").style ="color:coral; border-color:coral;"
+    }
 });
+
+
 
 document.addEventListener("keyup", (event) => {
     if(event.key == "Escape"){
         document.getElementById("esc").style = "color:dodgerblue; border-color:dodgerblue;"
+    }
+
+    else if(event.key == "F1"){
+        document.getElementById("f1").style = "color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "F2"){
+        document.getElementById("f2").style = "color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "F3"){
+        document.getElementById("f3").style = "color:dodger-blue; border-color:dodger-blue;"
+    }    
+    
+    else if(event.key == "F4"){
+        document.getElementById("f4").style = "color:dodger-blue; border-color:dodger-blue;"
+    }    
+    
+    else if(event.key == "F5"){
+        document.getElementById("f5").style = "color:dodger-blue; border-color:dodger-blue;"
+    }    
+    
+    else if(event.key == "F6"){
+        document.getElementById("f6").style = "color:dodger-blue; border-color:dodger-blue;"
+    }    
+    
+    else if(event.key == "F7"){
+        document.getElementById("f7").style = "color:dodger-blue; border-color:dodger-blue;"
+    }    
+    
+    else if(event.key == "F8"){
+        document.getElementById("f8").style = "color:dodger-blue; border-color:dodger-blue;"
+    }    
+    
+    else if(event.key == "F9"){
+        document.getElementById("f9").style = "color:dodger-blue; border-color:dodger-blue;"
+    }    
+    
+    else if(event.key == "F10"){
+        document.getElementById("f10").style = "color:dodger-blue; border-color:dodger-blue;"
+    }    
+    
+    else if(event.key == "F11"){
+        document.getElementById("f11").style = "color:dodger-blue; border-color:dodger-blue;"
+    }    
+    
+    else if(event.key == "F12"){
+        document.getElementById("f12").style = "color:dodger-blue; border-color:dodger-blue;"
+    }    
+    
+    else if(event.key == "PrintScreen"){
+        document.getElementById("prt").style = "color:dodger-blue; border-color:dodger-blue;"
+    }    
+    
+    else if(event.key == "Power"){
+        document.getElementById("pwr").style = "color:dodger-blue; border-color:dodger-blue;"
+    }    
+    
+    else if(event.key == "Delete"){
+        document.getElementById("del").style = "color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "§"){
+        document.getElementById("§").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "1"){
+        document.getElementById("1").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "2"){
+        document.getElementById("2").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "3"){
+        document.getElementById("3").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "4"){
+        document.getElementById("4").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "5"){
+        document.getElementById("5").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "6"){
+        document.getElementById("6").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "7"){
+        document.getElementById("7").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "8"){
+        document.getElementById("8").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "9"){
+        document.getElementById("9").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "0"){
+        document.getElementById("0").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "+"){
+        document.getElementById("plus").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == ""){
+        document.getElementById("accent").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "Backspace"){
+        document.getElementById("bck").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "Home"){
+        document.getElementById("home").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "Tab"){
+        document.getElementById("tab").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "q"){
+        document.getElementById("q").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "w"){
+        document.getElementById("w").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "e"){
+        document.getElementById("e").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "r"){
+        document.getElementById("r").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "t"){
+        document.getElementById("t").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "y"){
+        document.getElementById("y").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "u"){
+        document.getElementById("u").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "i"){
+        document.getElementById("i").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "o"){
+        document.getElementById("o").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "p"){
+        document.getElementById("p").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "å"){
+        document.getElementById("å").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == ""){
+        document.getElementById("¨").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "Enter"){
+        document.getElementById("enter").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "PageUp"){
+        document.getElementById("pg_up").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "CapsLock"){
+        document.getElementById("cps_lck").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "a"){
+        document.getElementById("a").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "s"){
+        document.getElementById("s").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "d"){
+        document.getElementById("d").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "f"){
+        document.getElementById("f").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "g"){
+        document.getElementById("g").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "h"){
+        document.getElementById("h").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "j"){
+        document.getElementById("j").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "k"){
+        document.getElementById("k").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "l"){
+        document.getElementById("l").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "ö"){
+        document.getElementById("ö").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "ä"){
+        document.getElementById("ä").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "'"){
+        document.getElementById("apostrof").style ="color:dodger-blue; border-color:dodger-blue;"
+    }
+
+    else if(event.key == "PageDown"){
+        document.getElementById("pg_dn").style ="color:dodger-blue; border-color:dodger-blue;"
     }
 })
